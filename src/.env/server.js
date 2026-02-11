e4eec6c2d3613f4df8f7127f6f4ea1531fcadfb5
lukasjc120 <143965229+lukasjc120@users.noreply.github.com>
2026-02-11 15:01:16 -0300

    Proyecto

---
+++ src/.env/server.js
@@ -0,0 +1,7 @@
+DB_NAME=ecommerce_pps
+DB_USER=root
+DB_PASSWORD=tu_password
+DB_HOST=localhost
+DB_DIALECT=mysql
+PORT=3000
+JWT_SECRET=secreto_pps
---
+++ src/models/user.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.ENUM("ADMIN", "USER"),
+    defaultValue: "USER"
+  }
+}, {
+  tableName: "users"
+});
+
+module.exports = User;
